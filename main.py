f1716cc9cc3a0b22632f371153ef154c86705005
pyataninastesha <pyataninastefa@yandex.ru>
2023-11-23 17:59:26 +0300

    4 commit

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+from ui_file import Ui_MainWindow
 
-class Example(QMainWindow):
+
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
         self.do_paint = False
 
     def initUI(self):
-        uic.loadUi("UI.ui", self)
+        # uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.paint)
 
     def paint(self):
@@ -28,8 +31,9 @@ class Example(QMainWindow):
         self.do_paint = False
 
     def draw(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
         for i in range(100):
+            qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255),
+                        random.randint(0, 255)))
             x = random.randint(20, 600)
             y = random.randint(20, 600)
             r = random.randint(100, 100)
