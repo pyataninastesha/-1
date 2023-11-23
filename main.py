c4545f2b12e33555c5154b6a3225b82a38f2d846
pyataninastesha <pyataninastefa@yandex.ru>
2023-11-23 17:38:36 +0300

    3 commit

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
+import sys
+import random
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.do_paint = False
+
+    def initUI(self):
+        uic.loadUi("UI.ui", self)
+        self.pushButton.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+        self.do_paint = False
+
+    def draw(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        for i in range(100):
+            x = random.randint(20, 600)
+            y = random.randint(20, 600)
+            r = random.randint(100, 100)
+            qp.drawEllipse(x, y, r, r)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
